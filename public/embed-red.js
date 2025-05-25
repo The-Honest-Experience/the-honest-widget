e146d9dd8e43876742cf450b19ee843aaf0870f6
The-Honest-Experience <thehonestexperience.team@gmail.com>
2025-05-25 07:55:31 +0200

    Update embed-red.js

--- public/embed-red.js
+++ public/embed-red.js
@@ -20,12 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
   <div class="the-honest-badge">
     <div class="the-score-block">
       <div class="the-score-row">
-        <img src="https://74b0fc046962dee287537fffacbddacd.cdn.bubble.io/f1744554362576x344039617658736400/Total-score-the-honest-experience.png" class="score-icon" alt="Score Icon">
+        <img src="https://74b0fc046962dee287537fffacbddacd.cdn.bubble.io/f1748152410553x159106384415167680/total-score-white-the-honest-experience.png?_gl=1*93u0pn*_gcl_au*MjA2MTY0NDE0MS4xNzQ2NTM4Nzcy*_ga*MjA5NDY1MjE0My4xNzQ2NTM4NzM1*_ga_BFPVR2DEE2*czE3NDgxNDgzMDAkbzE2JGcxJHQxNzQ4MTUyMTU4JGo0NyRsMCRoMCRkamRqak14OVhkZmRVdjNYSVdlXzk2a2N5RmgtSm1IN1hkdw.." class="score-icon" alt="Score Icon">
         <div class="the-honest-score">${score.toFixed(1)}</div>
       </div>
       <div class="the-honest-reviews">${total_reviews} verified review${total_reviews !== 1 ? 's' : ''}</div>
     </div>
-    <img src="https://74b0fc046962dee287537fffacbddacd.cdn.bubble.io/f1743717117168x981686514746771100/LOGO-3-thehonestexperience.png?_gl=1*1dniwaj*_gcl_au*MjA2MTY0NDE0MS4xNzQ2NTM4Nzcy*_ga*MjA5NDY1MjE0My4xNzQ2NTM4NzM1*_ga_BFPVR2DEE2*czE3NDc4Njg5OTUkbzE1JGcxJHQxNzQ4MDk2NTg2JGozMCRsMCRoMCRkamRqak14OVhkZmRVdjNYSVdlXzk2a2N5RmgtSm1IN1hkdw.." class="honest-logo" alt="The Honest Experience">
+    <img src="https://74b0fc046962dee287537fffacbddacd.cdn.bubble.io/f1748152500612x296252883912272640/Logo_THE_weiss.png?_gl=1*13399zk*_gcl_au*MjA2MTY0NDE0MS4xNzQ2NTM4Nzcy*_ga*MjA5NDY1MjE0My4xNzQ2NTM4NzM1*_ga_BFPVR2DEE2*czE3NDgxNDgzMDAkbzE2JGcxJHQxNzQ4MTUyMTU4JGo0NyRsMCRoMCRkamRqak14OVhkZmRVdjNYSVdlXzk2a2N5RmgtSm1IN1hkdw.." class="honest-logo" alt="The Honest Experience">
   </div>
 
         `;
